ab6764f71cca19c862228b2511e2be2aadbc64eb
Joel Straley <joelstraley@gmail.com>
2023-10-04 01:21:06 -0400

    updating config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'AIdiner' );
+define( 'DB_NAME', 'dcv0qvpmqd4m1p' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'hsrbfnlnshrvpr' );
 
 /** Database password */
 define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', 'aqueous-gorge-97849-8376181056e6.herokuapp.com/:5432' );
+define( 'DB_HOST', 'ec2-107-21-67-46.compute-1.amazonaws.com:5432' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
